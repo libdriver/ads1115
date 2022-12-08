15250593bcc3be050a582521ad3fd5bb3ef446ed
e <lishifenging@outlook.com>
2022-12-09 02:42:32 +0800

    docs: add CONTRIBUTING.md

--- doc/html/annotated_dup.js
+++
@@ -1,5 +0,0 @@
-var annotated_dup =
-[
-    [ "ads1115_handle_s", "structads1115__handle__s.html", "structads1115__handle__s" ],
-    [ "ads1115_info_s", "structads1115__info__s.html", "structads1115__info__s" ]
-];
--- doc/html/driver__ads1115__register__test_8c.js
+++
@@ -1,4 +0,0 @@
-var driver__ads1115__register__test_8c =
-[
-    [ "ads1115_register_test", "group__ads1115__test__driver.html#gaab3ab3be91a856143a08e26f7a2a2be9", null ]
-];

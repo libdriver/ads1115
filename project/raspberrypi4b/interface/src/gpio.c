54abdec5046051b458de880f5a84baaf39b26f72
lishifeng <lishifenging@outlook.com>
2025-03-02 13:50:06 +0800

    fix doc errors

--- project/raspberrypi4b/interface/src/gpio.c
+++ project/raspberrypi4b/interface/src/gpio.c
@@ -58,7 +58,7 @@ extern volatile uint8_t g_flag;           /**< interrupt flag */
 
 /**
  * @brief  gpio interrupt pthread
- * @param  *p ponts to an args buffer
+ * @param  *p pointer to an args buffer
  * @return NULL
  * @note   none
  */
